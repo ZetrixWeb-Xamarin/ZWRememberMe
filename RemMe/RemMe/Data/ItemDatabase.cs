6999e4b64d86d4c9cb204657ab6be195fab1a312
ZetrixWeb-Xamarin <xamarian.zetrixweb@gmail.comxamarian.zetrixweb@gmail.com>
2016-12-25 17:54:18 +0530

    First Commit
    First Commit

---
+++ RemMe/RemMe/Data/ItemDatabase.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
+using SQLite.Net;
+using RememberMe.Models;
+
+namespace RememberMe.Data
+{
+    public class ItemDatabase
+    {
+        static object locker = new object();
+
+        SQLiteConnection database;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Tasky.DL.TaskDatabase"/> TaskDatabase. 
+        /// if the database doesn't exist, it will create the database and all the tables.
+        /// </summary>
+        /// <param name='path'>
+        /// Path.
+        /// </param>
+        public ItemDatabase()
+        {
+            database = DependencyService.Get<ISQLite>().GetConnection();
+            // create the tables
+            database.CreateTable<Item>();
+        }
+        
+        public IEnumerable<Item> GetItems(string search = null)
+        {
+
+            string query = "SELECT * FROM [Item]";
+
+            if (!string.IsNullOrEmpty(search))
+            {
+                query += " WHERE Name like '%" + search + "%'";
+            }
+
+            lock (locker)
+            {
+                return database.Query<Item>(query);
+            }
+        }
+
+        public IEnumerable<Reminder> GetReminders()
+        {
+            lock (locker)
+            {
+                return database.Table<Reminder>().ToList();
+            }
+        }
+
+        public Item GetItem(int id)
+        {
+            lock (locker)
+            {
+                return database.Table<Item>().FirstOrDefault(x => x.ID == id);
+            }
+        }
+
+        public Reminder GetReminder(int id)
+        {
+            lock (locker)
+            {
+                return database.Table<Reminder>().FirstOrDefault(x => x.ID == id);
+            }
+        }
+
+        public int SaveItem(Item item)
+        {
+            lock (locker)
+            {
+                if (item.ID != 0)
+                {
+                    database.Update(item);
+                    return item.ID;
+                }
+                else
+                {
+                    return database.Insert(item);
+                }
+            }
+        }
+
+        public int SaveReminder(Reminder reminder)
+        {
+            lock (locker)
+            {
+                if (reminder.ID != 0)
+                {
+                    database.Update(reminder);
+                    return reminder.ID;
+                }
+                else
+                {
+                    return database.Insert(reminder);
+                }
+            }
+        }
+
+        public int DeleteItem(int id)
+        {
+            lock (locker)
+            {
+                return database.Delete<Item>(id);
+            }
+        }
+
+        public int DeleteReminder(int id)
+        {
+            lock (locker)
+            {
+                return database.Delete<Reminder>(id);
+            }
+        }
+    }
+}
+
---
+++ RemMe/RemMe/Pages/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RememberMe.Pages
+{
+    public partial class MainPage : ContentPage
+    {
+
+        private TapGestureRecognizer CollectRecognizer = new TapGestureRecognizer();
+        private TapGestureRecognizer SearchRecognizer = new TapGestureRecognizer();
+        private TapGestureRecognizer ReminderRecognizer = new TapGestureRecognizer();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            CollectRecognizer.Tapped += CollectRecognizer_Tapped;
+            Collect.GestureRecognizers.Add(CollectRecognizer);
+
+            SearchRecognizer.Tapped += SearchRecognizer_Tapped;
+            Search.GestureRecognizers.Add(SearchRecognizer);
+
+            ReminderRecognizer.Tapped += ReminderRecognizer_Tapped;
+            Reminder.GestureRecognizers.Add(ReminderRecognizer);
+        }
+
+        private void ReminderRecognizer_Tapped(object sender, EventArgs e)
+        {
+            App.Current.Navigation.PushAsync(new ReminderPage());
+        }
+
+        private void SearchRecognizer_Tapped(object sender, EventArgs e)
+        {
+            App.Current.Navigation.PushAsync(new SearchPage());
+        }
+
+        private void CollectRecognizer_Tapped(object sender, EventArgs e)
+        {
+          PopupNavigation.PushAsync(new RegisterPage()); 
+        }
+    }
+}
